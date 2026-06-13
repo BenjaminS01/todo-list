9e32aff896156b5bad0e1ddc2bdcbb736f7bef98
userpc <bswarovsky@gmx.net>
2026-06-14 00:33:45 +0200

    controller tests, cucumber, swagger

--- backend/src/main/java/com/bs/todo/todo/infrastructure/api/controller/TodoItemController.java
+++ backend/src/main/java/com/bs/todo/todo/infrastructure/api/controller/TodoItemController.java
@@ -5,6 +5,8 @@ import com.bs.todo.todo.application.service.TodoItemService;
 import com.bs.todo.todo.domain.TodoItem;
 import com.bs.todo.todo.infrastructure.api.dto.TodoItemRequest;
 import com.bs.todo.todo.infrastructure.api.dto.TodoItemResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,12 +26,17 @@ public class TodoItemController {
     }
 
     @PostMapping
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "TODO entry created"),
+            @ApiResponse(responseCode = "400", description = "Invalid request body")
+    })
     public ResponseEntity<TodoItemResponse> create(@Valid @RequestBody TodoItemRequest request) {
         TodoItem created = service.create(request.getDescription(), request.getCompletionDate());
         return ResponseEntity.status(HttpStatus.CREATED).body(toResponse(created));
     }
 
     @GetMapping
+    @ApiResponse(responseCode = "200", description = "List of all TODO entries")
     public ResponseEntity<List<TodoItemResponse>> findAll() {
         List<TodoItemResponse> response = service.findAll()
                 .stream()
@@ -39,6 +46,10 @@ public class TodoItemController {
     }
 
     @DeleteMapping("/{id}")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "TODO entry deleted"),
+            @ApiResponse(responseCode = "404", description = "TODO entry not found")
+    })
     public ResponseEntity<Void> delete(@PathVariable UUID id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
