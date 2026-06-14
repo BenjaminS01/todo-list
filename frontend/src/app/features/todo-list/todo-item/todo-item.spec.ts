8eca3fe0ad27f7d5f871439d78c6d6279bcd56ce
userpc <bswarovsky@gmx.net>
2026-06-14 21:47:46 +0200

    US2 todo components created

--- frontend/src/app/features/todo-list/todo-item/todo-item.spec.ts
+++ frontend/src/app/features/todo-list/todo-item/todo-item.spec.ts
@@ -1,22 +1,47 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TodoItem } from './todo-item';
+import { TodoItemComponent } from './todo-item';
+import { TodoItem } from '../../../core/todo-item/todo-item.model';
 
 describe('TodoItem', () => {
-  let component: TodoItem;
-  let fixture: ComponentFixture<TodoItem>;
+  let component: TodoItemComponent;
+  let fixture: ComponentFixture<TodoItemComponent>;
+
+  const mockItem: TodoItem = {
+    id: '1',
+    description: 'Öl wechsel',
+    completionDate: new Date(2026, 5, 20)
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TodoItem],
+      imports: [TodoItemComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(TodoItem);
+    fixture = TestBed.createComponent(TodoItemComponent);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.componentRef.setInput('item', mockItem);
+    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display description', () => {
+    expect(fixture.nativeElement.textContent).toContain('Öl wechsel');
+  });
+
+  it('should display completionDate formatted as DD.MM.YYYY', () => {
+    expect(fixture.nativeElement.textContent).toContain('20.06.2026');
+  });
+
+  it('should emit item id when delete button is clicked', () => {
+    let emittedId: string | undefined;
+    component.delete.subscribe((id: string) => emittedId = id);
+
+    fixture.nativeElement.querySelector('button').click();
+
+    expect(emittedId).toBe('1');
   });
 });
