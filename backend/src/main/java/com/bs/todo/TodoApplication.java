2c4779da7da44596d80260c530378002c94eb668
userpc <bswarovsky@gmx.net>
2026-06-14 23:14:56 +0200

    US1 webconfig and testdata

--- backend/src/main/java/com/bs/todo/TodoApplication.java
+++ backend/src/main/java/com/bs/todo/TodoApplication.java
@@ -1,7 +1,12 @@
 package com.bs.todo;
 
+import com.bs.todo.todo.application.service.TodoItemService;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class TodoApplication {
@@ -10,4 +15,20 @@ public class TodoApplication {
 		SpringApplication.run(TodoApplication.class, args);
 	}
 
+	@Bean
+    ApplicationRunner dataInitializer(TodoItemService service) {
+		return args -> {
+			service.create("Öl wechsel", LocalDate.of(2026, 6, 20));
+			service.create("Rasen mähen", LocalDate.of(2026, 6, 25));
+			service.create("Einkaufen", LocalDate.of(2026, 6, 15));
+			service.create("10 Liegestütze", LocalDate.of(2026, 7, 1));
+			service.create("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod " +
+					"tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam" +
+					" et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem " +
+					"ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy " +
+					"eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et " +
+					"accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est " +
+					"Lorem ipsum dolor sit amet.", LocalDate.of(2026, 7, 10));
+		};
+	}
 }
